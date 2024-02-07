97f62672fe07f88bb893b127b9335be3202b4535
omiddd <omidrazzaghi2000@gmail.com>
2024-02-07 23:24:33 +0330

    server send turn

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import time
 ADDRESS = socket.gethostname()
 PORT    = 12346
 
+client_conn_array = []
+
 def acceptClients():
     #define socket
     serverSocket = socket.socket()
@@ -15,19 +17,34 @@ def acceptClients():
     #listen 
     serverSocket.listen(2)
 
-    #always waiting for connection
-
+    
     while True:
+
         print("Waiting for new connection ")
 
         clientConnection,address = serverSocket.accept()
 
+        client_conn_array.append(clientConnection)
+
+        while(len(client_conn_array) == 2):
+            
+            client_conn_array[0].send(b"It is your turn ... say a cell number")
+            
+            print(f"Player1 said f{client_conn_array[0].recv()}")
+
+            client_conn_array[1].send(b"It is your turn ... say a cell number")
+            
+            print(f"Player2 said f{client_conn_array[1].recv()}")
+
+
+
+
+
         print(f"new Client with {address}")
+
+        
 
 
 acceptClients_thread = threading.Thread(target=acceptClients,args=())
 acceptClients_thread.start()
-
-
-while True:
-    time.sleep(1)
+acceptClients_thread.join()
