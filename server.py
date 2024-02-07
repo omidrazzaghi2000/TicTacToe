7f853fd24387b6bf446d6657ae4b545b95b99a1e
omiddd <omidrazzaghi2000@gmail.com>
2024-02-07 23:55:23 +0330

    add 3x3 and 4x4 and 5x5 table and threads

--- server.py
+++ server.py
@@ -3,16 +3,28 @@ import threading
 import time
 
 ADDRESS = socket.gethostname()
-PORT    = 12346
+PORT_3X3    = 12346
+PORT_4X4    = 12347
+PORT_5X5    = 12348
 
-client_conn_array = []
+client_conn_array_3x3 = []
+client_conn_array_4x4 = []
+client_conn_array_5x5 = []
 
-def acceptClients():
+def acceptClients(board_size):
     #define socket
     serverSocket = socket.socket()
 
+    #reuse address option
+    serverSocket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+
     #bind socket
-    serverSocket.bind((ADDRESS,PORT))
+    if board_size == 3:
+        serverSocket.bind((ADDRESS,PORT_3X3))
+    elif board_size == 4:
+        serverSocket.bind((ADDRESS,PORT_4X4))
+    elif board_size == 5:
+        serverSocket.bind((ADDRESS,PORT_5X5))
 
     #listen 
     serverSocket.listen(2)
@@ -20,31 +32,44 @@ def acceptClients():
     
     while True:
 
-        print("Waiting for new connection ")
+        print(f"Waiting for new connection for board size : {board_size}")
 
         clientConnection,address = serverSocket.accept()
+
+        if(board_size == 3):
+            client_conn_array = client_conn_array_3x3
+        elif(board_size == 4):
+            client_conn_array = client_conn_array_4x4
+        elif(board_size == 5):
+            client_conn_array = client_conn_array_5x5
 
         client_conn_array.append(clientConnection)
 
         while(len(client_conn_array) == 2):
             
-            client_conn_array[0].send(b"It is your turn ... say a cell number")
+            client_conn_array[0].send(f"It is your turn on board {board_size}x{board_size} ... say a cell number".encode())
             
-            print(f"Player1 said f{client_conn_array[0].recv()}")
+            print(f"Player1 on board {board_size}x{board_size} said {client_conn_array[0].recv(1024).decode()}")
 
-            client_conn_array[1].send(b"It is your turn ... say a cell number")
+            client_conn_array[1].send(f"It is your turn on board {board_size}x{board_size} ... say a cell number".encode())
             
-            print(f"Player2 said f{client_conn_array[1].recv()}")
-
-
-
+            print(f"Player2 on board {board_size}x{board_size} said {client_conn_array[1].recv(1024).decode()}")
 
 
         print(f"new Client with {address}")
 
+
+
+
         
 
 
-acceptClients_thread = threading.Thread(target=acceptClients,args=())
-acceptClients_thread.start()
-acceptClients_thread.join()
+acceptClients3x3_thread = threading.Thread(target=acceptClients,args=(3,))
+acceptClients3x3_thread.start()
+acceptClients4x4_thread = threading.Thread(target=acceptClients,args=(4,))
+acceptClients4x4_thread.start()
+acceptClients5x5_thread = threading.Thread(target=acceptClients,args=(5,))
+acceptClients5x5_thread.start()
+acceptClients3x3_thread.join()
+acceptClients4x4_thread.join()
+acceptClients5x5_thread.join()
