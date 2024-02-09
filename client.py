dbca15e7c69e3e8f5754053871864519234a28f4
omidrazzaghi2000 <55230497+omidrazzaghi2000@users.noreply.github.com>
2024-02-09 17:48:18 +0330

    update client code

--- client.py
+++ client.py
@@ -7,12 +7,20 @@ SERVER_PORT    = 12346
 clientsocket = socket.socket()
 
 clientsocket.connect((SERVER_ADDRESS,SERVER_PORT))
-message = input("choose your board size: ")
-clientsocket.send(message.encode())
+board_size=['3','4','5']
+message=''
+data=''
+
+while(data=="You can not join to the game because this board is full" or message not in board_size):
+ message = input("choose your board size: ")
+ clientsocket.send(message.encode())
+ data=clientsocket.recv(1024).decode()
+ print(data)
+
 
 while True:
         data = clientsocket.recv(1024).decode()  # receive response
 
         print('Received from server: ' + data)  # show in terminal
         message = input(" -> ")  # again take input
-        clientsocket.send(message.encode())  #
+        clientsocket.send(message.encode())  # send message
